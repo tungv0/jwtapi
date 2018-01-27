b485c6604cc883d953e2a269ddf985893ed9e0b0
Tung Vo <tuvo@c02tp29xhf1r-m.lan>
2018-01-27 14:54:01 -0800

    complete auth

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+var router = express.Router();
+var bodyParser = require('body-parser');
+
 var UserController = require('./user/UserController');
 var AuthController = require('./auth/AuthController');
 
 app.use('/users', UserController);
 app.use('/api/auth', AuthController);
+
+app.use('/', router.get('/', function (req, res) {
+    console.log("Serving root request");
+    res.status(200).send('Hello World');
+}))
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://tungtanvo:tungv082@ds211558.mlab.com:11558/jwtapi', { useMongoClient: true });
+mongoose.connect('mongodb://tung:tungv082@ds211558.mlab.com:11558/jwtapi', { useMongoClient: true });
